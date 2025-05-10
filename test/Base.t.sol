88472d99e30757a581226ce7ca24a83a20eedc35
Cooki0x <cooki@defi.sucks>
2025-05-10 15:27:50 +1000

    feat: add corn network

--- test/Base.t.sol
+++ test/Base.t.sol
@@ -4,6 +4,9 @@ pragma solidity 0.8.20;
 import {Test} from 'forge-std/Test.sol';
 
 contract Base is Test {
-  function setUp() external {}
+  function setUp() external {
+    vm.createSelectFork(vm.rpcUrl('corn'));
+  }
+
   function testInit() external {}
 }
