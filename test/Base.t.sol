cf63d85b2a6d56bc238f080c4abc66ffbee512f0
Cooki0x <cooki@defi.sucks>
2025-05-10 14:39:15 +1000

    feat: remove scripts and tests

---
+++ test/Base.t.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.20;
+
+import {Test} from 'forge-std/Test.sol';
+
+contract Base is Test {
+  function setUp() external {}
+  function testInit() external {}
+}
